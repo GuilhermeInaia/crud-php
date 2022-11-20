220324d1dfb3601e1882ded2623d2e7a6ff91d30
guilhermeinaia <guilherme.inaias@gmail.com>
2022-11-20 17:07:27 -0300

    Atualizando

--- src/connection/conexao.php
+++ src/connection/conexao.php
@@ -4,7 +4,7 @@ function abrirConexao(): PDO
 {
     $Host = 'localhost';
     $UserName = 'root';
-    $Password = '@2018#2227';
+    $Password = '';
     $DataBase = 'db';
 
     $conexao = new PDO("mysql:host={$Host};dbname={$DataBase}", $UserName, $Password);
